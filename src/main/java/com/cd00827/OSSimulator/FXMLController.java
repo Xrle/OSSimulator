17a1cb39e4965d5043b39125f21f616367f286aa
Xrle <christopherdigby@gmail.com>
2020-05-03 15:35:19 +0100

    Main I/O on UI

--- src/main/java/com/cd00827/OSSimulator/FXMLController.java
+++ src/main/java/com/cd00827/OSSimulator/FXMLController.java
@@ -5,13 +5,27 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
 
 public class FXMLController implements Initializable {
 
     @FXML
     private ListView input;
+    @FXML
+    private ListView output;
+    @FXML
+    private ListView execTrace;
+    @FXML
+    private Button add;
+    @FXML
+    private Button remove;
+    @FXML
+    private Button execute;
+    @FXML
+    private Button clear;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -31,6 +45,16 @@ public class FXMLController implements Initializable {
         input.getItems().add(":))");
         input.getItems().add(":))");
         input.getItems().add(":))");
-
+        output.getItems().add("test");
+        output.getItems().add("test");
+        output.getItems().add("test");
+        output.getItems().add("test");
+        output.getItems().add("test");
+        execTrace.getItems().add("cheese");
+        execTrace.getItems().add("cheese");
+        execTrace.getItems().add("cheese");
+        execTrace.getItems().add("cheese");
+        execTrace.getItems().add("cheese");
+        execTrace.getItems().add("cheese");
     }
 }
